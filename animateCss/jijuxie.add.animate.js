b30170d2406f4f00528f9613ed19f211d7cdbc18
unknown <>
2017-03-10 23:30:11 +0800

    1

---
+++ animateCss/jijuxie.add.animate.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 12156 on 2017/3/10.
+ */
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
